0833a6e69459bbe0940e64c37989f102d7fc116c
Dan Maas <dmaas@maasdigital.com>
2021-03-01 11:45:12 -0800

    updates for Terraform v0.13

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
 output "vpc_id" {
-  value = "${aws_vpc.default.id}"
+  value = aws_vpc.default.id
 }
 
 output "ready" {
-  value = "${join(",",aws_route_table_association.public.*.id)}"
+  value = join(",",aws_route_table_association.public.*.id)
 }
 
 output "public_subnet_id0" {
   # work-around for Terraform bug, see http://dexlab.net/pukiwiki/index.php?cmd=read&page=Memo%2FTerraform
 #  value = "${aws_subnet.public.0.id}"
-  value = "${element(aws_subnet.public.*.id, 0)}"
+  value = element(aws_subnet.public.*.id, 0)
 }
 output "public_subnet_id1" {
   # work-around for Terraform bug, see http://dexlab.net/pukiwiki/index.php?cmd=read&page=Memo%2FTerraform
 #  value = "${aws_subnet.public.1.id}"
-  value = "${element(aws_subnet.public.*.id, 1)}"
+  value = element(aws_subnet.public.*.id, 1)
 }
 output "public_subnet_ids" {
-  value = "${aws_subnet.public.*.id}"
+  value = aws_subnet.public.*.id
 }
 
 output "lambda_subnet_id0" {
@@ -38,21 +38,21 @@ output "nat_gateway_public_ips" {
 }
 
 output "db_subnet_group_id" {
-  value = "${aws_db_subnet_group.db_subnet_group.id}"
+  value = aws_db_subnet_group.db_subnet_group.id
 }
 
 output "ssh_access_security_group_id" {
-  value = "${aws_security_group.ssh_access.id}"
+  value = aws_security_group.ssh_access.id
 }
 
 output "fe_elb_security_group_id" {
-  value = "${join(",", aws_security_group.fe_elb.*.id)}"
+  value = join(",", aws_security_group.fe_elb.*.id)
 }
 
 output "fe_haproxy_security_group_id" {
-  value = "${join(",", aws_security_group.fe_haproxy.*.id)}"
+  value = join(",", aws_security_group.fe_haproxy.*.id)
 }
 
 output "private_dns_zone_id" {
-  value = "${join(",", aws_route53_zone.private_dns.*.zone_id)}"
+  value = join(",", aws_route53_zone.private_dns.*.zone_id)
 }
--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "default" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   tags = map(
-      "Name", "${var.sitename}",
+      "Name", var.sitename,
       "Terraform", "true",
       "kubernetes.io/cluster/${var.sitename}", "shared",
       )
